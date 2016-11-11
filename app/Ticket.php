9ca3a58c870af1c4e702eea38f1396c216fd5277
fumbaa <fumba.me@gmail.com>
2016-11-11 17:03:23 -0500

    Submitting to db

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-    //
+     protected $fillable = ['title', 'content', 'slug', 'status', 'user_id'];
+	 
+	 //alternative
+	 //protected $guarded = ['id'];
 }
--- resources/views/tickets/create.blade.php
+++ resources/views/tickets/create.blade.php
@@ -10,6 +10,12 @@
 				@foreach ($errors->all() as $error)
 					<p class="alert alert-danger">{{ $error }}</p>
 				@endforeach
+				
+				@if (session('status'))
+					<div class="alert alert-success">
+						{{ session('status') }}
+					</div>
+				@endif
 
                 <fieldset>
                     <legend>Submit a new ticket</legend>
