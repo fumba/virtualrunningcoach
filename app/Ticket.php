b92b255e7cb8aa8e547f0cad6d68d849c1acb98d
fumbaa <fumba.me@gmail.com>
2016-11-14 22:19:45 -0500

    Comments can now be added

--- app/Ticket.php
+++ app/Ticket.php
@@ -6,8 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Ticket extends Model
 {
-     protected $fillable = ['title', 'content', 'slug', 'status', 'user_id'];
+	//alternative
+    //protected $fillable = ['title', 'content', 'slug', 'status', 'user_id'];
 	 
-	 //alternative
-	 //protected $guarded = ['id'];
+	protected $guarded = ['id'];
+
+	public function comments()
+	{
+		return $this->hasMany('App\Comment', 'post_id');
+	}
+
 }
