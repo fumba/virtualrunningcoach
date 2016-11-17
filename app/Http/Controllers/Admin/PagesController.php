a94abca6a5cf5761d3c9698b4d9e875fca50293e
fumbaa <fumba.me@gmail.com>
2016-11-16 23:08:53 -0500

    Removed unused imports

--- app/Http/Controllers/Admin/PagesController.php
+++ app/Http/Controllers/Admin/PagesController.php
@@ -2,15 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use Illuminate\Http\Request;
-
-use App\Http\Requests;
 use App\Http\Controllers\Controller;
 
-class PagesController extends Controller
-{
-    public function home()
-    {
-        return view('backend.home');
-    }
+class PagesController extends Controller {
+	public function home() {
+		return view ( 'backend.home' );
+	}
 }
--- routes/web.php
+++ routes/web.php
@@ -1,55 +1,58 @@
 <?php
 
 /*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+ * |--------------------------------------------------------------------------
+ * | Web Routes
+ * |--------------------------------------------------------------------------
+ * |
+ * | Here is where you can register web routes for your application. These
+ * | routes are loaded by the RouteServiceProvider within a group which
+ * | contains the "web" middleware group. Now create something great!
+ * |
+ */
+Route::get ( '/', 'PagesController@home' );
+Route::get ( 'home', 'PagesController@home' );
 
-Route::get('/', 'PagesController@home');
-Route::get('home', 'PagesController@home');
+Route::get ( '/about', 'PagesController@about' );
+Route::get ( '/contact', 'TicketsController@create' );
+Route::post ( '/contact', 'TicketsController@store' );
+Route::get ( '/tickets', 'TicketsController@index' );
+Route::get ( '/ticket/{slug?}', 'TicketsController@show' );
+Route::get ( '/ticket/{slug?}/edit', 'TicketsController@edit' );
+Route::post ( '/ticket/{slug?}/edit', 'TicketsController@update' );
+Route::post ( '/ticket/{slug?}/delete', 'TicketsController@destroy' );
 
-Route::get('/about', 'PagesController@about');
-Route::get('/contact', 'TicketsController@create');
-Route::post('/contact', 'TicketsController@store');
-Route::get('/tickets', 'TicketsController@index');
-Route::get('/ticket/{slug?}', 'TicketsController@show');
-Route::get('/ticket/{slug?}/edit','TicketsController@edit');
-Route::post('/ticket/{slug?}/edit','TicketsController@update');
-Route::post('/ticket/{slug?}/delete','TicketsController@destroy');
+Route::post ( '/comment', 'CommentsController@newComment' );
 
-Route::post('/comment', 'CommentsController@newComment');
+Route::get ( 'users/register', 'Auth\RegisterController@showRegistrationForm' );
+Route::post ( 'users/register', 'Auth\RegisterController@register' );
+Route::get ( 'users/logout', 'Auth\LoginController@logout' );
 
-Route::get('users/register', 'Auth\RegisterController@showRegistrationForm');
-Route::post('users/register', 'Auth\RegisterController@register');
-Route::get('users/logout', 'Auth\LoginController@logout');
+Route::get ( 'users/login', 'Auth\LoginController@showLoginForm' );
+Route::post ( 'users/login', 'Auth\LoginController@login' );
 
-Route::get('users/login', 'Auth\LoginController@showLoginForm');
-Route::post('users/login', 'Auth\LoginController@login');
-
-Route::group(array('prefix' => 'admin', 'namespace' => 'Admin', 'middleware' => 'manager'), function () {
-    Route::get('users', 'UsersController@index');
-	Route::get('roles', 'RolesController@index');
-    Route::get('roles/create', 'RolesController@create');
-    Route::post('roles/create', 'RolesController@store');
-	Route::get('users/{id?}/edit', 'UsersController@edit');
-	Route::post('users/{id?}/edit','UsersController@update');
-	Route::get('/', 'PagesController@home');
+Route::group ( array (
+		'prefix' => 'admin',
+		'namespace' => 'Admin',
+		'middleware' => 'manager' 
+), function () {
+	Route::get ( 'users', 'UsersController@index' );
+	Route::get ( 'roles', 'RolesController@index' );
+	Route::get ( 'roles/create', 'RolesController@create' );
+	Route::post ( 'roles/create', 'RolesController@store' );
+	Route::get ( 'users/{id?}/edit', 'UsersController@edit' );
+	Route::post ( 'users/{id?}/edit', 'UsersController@update' );
+	Route::get ( '/', 'PagesController@home' );
 	
-	Route::get('posts', 'PostsController@index');
-	Route::get('posts/create', 'PostsController@create');
-	Route::post('posts/create', 'PostsController@store');
-	Route::get('posts/{id?}/edit', 'PostsController@edit');
-	Route::post('posts/{id?}/edit','PostsController@update');
+	Route::get ( 'posts', 'PostsController@index' );
+	Route::get ( 'posts/create', 'PostsController@create' );
+	Route::post ( 'posts/create', 'PostsController@store' );
+	Route::get ( 'posts/{id?}/edit', 'PostsController@edit' );
+	Route::post ( 'posts/{id?}/edit', 'PostsController@update' );
 	
-	Route::get('categories', 'CategoriesController@index');
-	Route::get('categories/create', 'CategoriesController@create');
-	Route::post('categories/create', 'CategoriesController@store');
-});
+	Route::get ( 'categories', 'CategoriesController@index' );
+	Route::get ( 'categories/create', 'CategoriesController@create' );
+	Route::post ( 'categories/create', 'CategoriesController@store' );
+} );
 
-Route::get('/blog', 'BlogController@index');
+Route::get ( '/blog', 'BlogController@index' );
