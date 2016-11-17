0e2804d86530a7f3df259f4a7964dd099483f2d7
fumbaa <fumba.me@gmail.com>
2016-11-17 18:33:47 -0500

    Using seeder for users table

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -1,16 +1,15 @@
 <?php
-
 use Illuminate\Database\Seeder;
-
-class DatabaseSeeder extends Seeder
-{
-    /**
-     * Run the database seeds.
-     *
-     * @return void
-     */
-    public function run()
-    {
-        // $this->call(UsersTableSeeder::class);
-    }
+use Illuminate\Database\Eloquent\Model;
+class DatabaseSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		Model::unguard ();
+		$this->call ( 'UserTableSeeder' );
+		Model::reguard ();
+	}
 }
---
+++ database/seeds/UserTableSeeder.php
@@ -0,0 +1,35 @@
+<?php
+use Illuminate\Database\Seeder;
+class UserTableSeeder extends Seeder {
+	/**
+	 * Run the database seeds.
+	 *
+	 * @return void
+	 */
+	public function run() {
+		DB::table ( 'users' )->insert ( [
+				[
+						'name' => 'Nathan',
+						'email' => 'test1@email.com',
+						'password' => bcrypt ( 'yourPassword' ),
+						'created_at' => new DateTime (),
+						'updated_at' => new DateTime ()
+				],
+				[
+						'name' => 'David',
+						'email' => 'test2@email.com',
+						'password' => bcrypt ( 'yourPassword' ),
+						'created_at' => new DateTime (),
+						'updated_at' => new DateTime ()
+				]
+				,
+				[
+						'name' => 'Lisa',
+						'email' => 'test3@email.com',
+						'password' => bcrypt ( 'yourPassword' ),
+						'created_at' => new DateTime (),
+						'updated_at' => new DateTime ()
+				]
+		] );
+	}
+}
